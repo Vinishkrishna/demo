c5378ab8f3d27d560a6c79a4077e859232961653
Vinishkrishna <vinishkrishna05@gmail.com>
2025-12-16 23:28:22 +0530

    commit

--- data.py
+++ data.py
@@ -1,15 +1,15 @@
 # data.py
 employees = {
-    "E001": {"name": "Rajesh Kumar", "efficiency": 0.95 , "trained_skils": "Paint_Booth,prefit"},   # 100% efficient
-    "E002": {"name": "Amita Sharma", "efficiency": 0.9, "trained_skils": "Paint_Booth,CCA"},
-    "E003": {"name": "Vikram Singh", "efficiency": 0.5, "trained_skils": "prefit,CCA"},
-    "E004": {"name": "Priya Patel", "efficiency": 0.8, "trained_skils": "PAA,CCA"},
-    "E005": {"name": "Anil Mehta", "efficiency": 0.75, "trained_skils": "Paint_Booth,Autoclave"},
-    "E006": {"name": "Sunita Reddy", "efficiency": 0.6, "trained_skils": "prefit,PAA"},
-    "E007": {"name": "Karan Johar", "efficiency": 0.85, "trained_skils": "Autoclave,CCA"},
-    "E008": {"name": "Neha Gupta", "efficiency": 0.7, "trained_skils": "Paint_Booth,PAA"},
-    "E009": {"name": "Ravi Das", "efficiency": 0.65, "trained_skils": "prefit,Autoclave"},
-    "E010":{"name": "Sonal Verma", "efficiency": 0.9, "trained_skils": "CCA,PAA"},
+    "E001": {"name": "Rajesh Kumar", "efficiency": 0.95 , "trained_skills": "Paint_Booth,prefit"},   # 100% efficient
+    "E002": {"name": "Amita Sharma", "efficiency": 0.9, "trained_skills": "Paint_Booth,CCA"},
+    "E003": {"name": "Vikram Singh", "efficiency": 0.5, "trained_skills": "prefit,CCA"},
+    "E004": {"name": "Priya Patel", "efficiency": 0.8, "trained_skills": "PAA,CCA"},
+    "E005": {"name": "Anil Mehta", "efficiency": 0.75, "trained_skills": "Paint_Booth,Autoclave"},
+    "E006": {"name": "Sunita Reddy", "efficiency": 0.6, "trained_skills": "prefit,PAA"},
+    "E007": {"name": "Karan Johar", "efficiency": 0.85, "trained_skills": "Autoclave,CCA"},
+    "E008": {"name": "Neha Gupta", "efficiency": 0.7, "trained_skills": "Paint_Booth,PAA"},
+    "E009": {"name": "Ravi Das", "efficiency": 0.65, "trained_skills": "prefit,Autoclave"},
+    "E010":{"name": "Sonal Verma", "efficiency": 0.9, "trained_skills": "CCA,PAA"},
 }
 
 parts = {
